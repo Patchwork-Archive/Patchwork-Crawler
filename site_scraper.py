4b386c853364deb891690b4d7d290d43ac5b80bc
Pinapelz <yukais@pinapelz.com>
2025-01-03 01:10:32 -0800

    add no-sandbox arg for running chromedriver as root

--- site_scraper.py
+++ site_scraper.py
@@ -19,6 +19,7 @@ class SiteScraper:
                 self.chrome_options.add_argument("--headless")
                 self.chrome_options.add_argument("--disable-gpu")
                 self.chrome_options.add_argument("--window-size=1920,1080")
+                self.chrome_options.add_argument("--no-sandbox")
 
             self.driver = webdriver.Chrome(service=self.service, options=self.chrome_options)
         except FileNotFoundError:
